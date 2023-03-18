edbcd0a9fb4cbb2679e2e5dabf0c1ea253328ebe
Pernilla Hällgren <pernillahallgren@PerHal-MacBook-Pro.local>
2023-03-18 15:00:05 +0100

    remove comments and console logs

--- src/components/BoardGame.tsx
+++ src/components/BoardGame.tsx
@@ -11,71 +11,66 @@ const BoardGame = () => {
   const [gameSolved, setGameSolved] = useState<boolean>(false);
   const [showInfo, setShowInfo] = useState<boolean>(false);
 
-  // SHUFFLE TILES
   const handleShuffle = () => {
     const shuffledNumbers = shuffleRandomOrder(randomNumbers);
     setRandomNumbers(shuffledNumbers);
     setIsRandomized(true);
-    const gameFinished = isGameSolved(shuffledNumbers)
-    setShowInfo(false)
-    if(!gameFinished) {
-      setGameSolved(false)
+    const gameFinished = isGameSolved(shuffledNumbers);
+    setShowInfo(false);
+    if (!gameFinished) {
+      setGameSolved(false);
     }
   };
 
-  // WIN GAME 
   const handleWinGame = () => {
-    setRandomNumbers(numbers)
-    setGameSolved(true)
+    setRandomNumbers(numbers);
+    setGameSolved(true);
   };
 
-  // SHOW INSTRUCTIONS
   const handleShowInstructions = () => {
-    setShowInfo(!showInfo)
-  }
+    setShowInfo(!showInfo);
+  };
 
-  // MOVE TILES
   const handleMoveTiles = (number: number, index: number) => {
-    let zeroIndex = randomNumbers.indexOf(0); // this is the higest index 15 and the empty slot
+    let zeroIndex = randomNumbers.indexOf(0);
     let valueIndex = randomNumbers.indexOf(number);
-    console.log({ zeroIndex });
-    console.log({ valueIndex });
 
     if (
-      Math.abs(valueIndex - zeroIndex) === 4 || // check vertical and returns the absolute value of a number
-      Math.abs(valueIndex - zeroIndex) === 1 // check horizontal 
+      Math.abs(valueIndex - zeroIndex) === 4 ||
+      Math.abs(valueIndex - zeroIndex) === 1
     ) {
       shiftTiles(valueIndex, zeroIndex);
     }
 
     if (
-      Math.abs(valueIndex - zeroIndex) === 8 || 
-      Math.abs(valueIndex - zeroIndex) === 2 
+      Math.abs(valueIndex - zeroIndex) === 8 ||
+      Math.abs(valueIndex - zeroIndex) === 2
     ) {
       shiftTiles(valueIndex, zeroIndex);
     }
 
     if (
-      Math.abs(valueIndex - zeroIndex) === 12 || 
-      Math.abs(valueIndex - zeroIndex) === 3 
+      Math.abs(valueIndex - zeroIndex) === 12 ||
+      Math.abs(valueIndex - zeroIndex) === 3
     ) {
       shiftTiles(valueIndex, zeroIndex);
     }
   };
 
-  // SHIFT TILES
   const shiftTiles = (valueIndex: number, zeroIndex: number) => {
     const shiftedTiles: number[] = [...randomNumbers];
 
-    [shiftedTiles[valueIndex], shiftedTiles[zeroIndex]] = [shiftedTiles[zeroIndex], shiftedTiles[valueIndex]];
+    [shiftedTiles[valueIndex], shiftedTiles[zeroIndex]] = [
+      shiftedTiles[zeroIndex],
+      shiftedTiles[valueIndex],
+    ];
     setRandomNumbers(shiftedTiles);
 
-    const gameFinished = isGameSolved(shiftedTiles)
-    if(gameFinished) {
-      setGameSolved(true)
+    const gameFinished = isGameSolved(shiftedTiles);
+    if (gameFinished) {
+      setGameSolved(true);
     }
   };
-  
 
   return (
     <div className="container">
@@ -83,7 +78,9 @@ const BoardGame = () => {
         <button className="btn" onClick={handleShuffle}>
           Shuffle
         </button>
-        <button className="btn" onClick={handleShowInstructions}>Instructions</button>
+        <button className="btn" onClick={handleShowInstructions}>
+          Instructions
+        </button>
       </div>
       {gameSolved && (
         <div>
@@ -91,11 +88,13 @@ const BoardGame = () => {
         </div>
       )}
       {showInfo ? (
-        <p className="board__instructions">To solve the 15 puzzle game, identify misplaced tiles, move them towards their 
-          correct position by clicking on adjacent tiles, and repeat until all tiles are in 
-          the correct positions. You win when all the tiles are in ascending order. 
-          Be patient and persistent.
-          <br/><br/>
+        <p className="board__instructions">
+          To solve the 15 puzzle game, identify misplaced tiles, move them
+          towards their correct position by clicking on adjacent tiles, and
+          repeat until all tiles are in the correct positions. You win when all
+          the tiles are in ascending order. Be patient and persistent.
+          <br />
+          <br />
           Good Luck!
         </p>
       ) : null}
@@ -104,11 +103,17 @@ const BoardGame = () => {
           <>
             <div className="board__wrapper">
               {randomNumbers.map((number, index) => (
-                <Tile key={number} number={number} onClick={() => handleMoveTiles(number, index)}/>
+                <Tile
+                  key={number}
+                  number={number}
+                  onClick={() => handleMoveTiles(number, index)}
+                />
               ))}
             </div>
             <div className="btn-section">
-              <button className="btn win" onClick={handleWinGame}>Solve the game</button>
+              <button className="btn win" onClick={handleWinGame}>
+                Solve the game
+              </button>
             </div>
           </>
         ) : (
--- src/helpers/helpers.tsx
+++ src/helpers/helpers.tsx
@@ -8,13 +8,8 @@ export const shuffleRandomOrder = (numArray: number[]): number[] => {
   return !isGameSolved(shuffledArray) ? shuffledArray : shuffleRandomOrder(shuffledArray)
 };
 
-// CHECK IF GAME IS SOLVED
 export const isGameSolved = (numArray: number[]) => {
   return (
-    // slice and create a shallow array from 0 index to second last index (15)
-    // then check if all the numb are in right order compared to index 
-    // check if last elemet in array is 0
-    // return true or false
     numArray.slice(0, -1).every((numArray, index) => numArray === index + 1) && 
     numArray[numArray.length] === 0
   )
